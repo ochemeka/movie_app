45baedf476f739e22157bf0cfff2b5210668c9f1
ochemeka <67810268+ochemeka@users.noreply.github.com>
2024-04-12 00:21:16 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
 import "./App.css";
 
-const API_URL = "http://www.omdbapi.com?apikey=a19804ff";
+const API_URL = "https://www.omdbapi.com?apikey=a19804ff";
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
