b31bb70401c518fa6a347ad878e74d2a881ec762
ochemeka <67810268+ochemeka@users.noreply.github.com>
2024-04-12 00:39:08 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    searchMovies("Batman");
+    searchMovies({movies});
   }, []);
 
   const searchMovies = async (title) => {
